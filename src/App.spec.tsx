ecfedfdae28bd138c0c2182cfe9db3d9e2ec4d66
zuies <zurabichkhikvadze66@gmail.com>
2023-05-06 11:46:28 +0400

    add vitest

---
+++ src/App.spec.tsx
@@ -0,0 +1,18 @@
+import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  it("Renders Hellow world!", () => {
+    // ARRANGE
+    render(<App />);
+    // ACT
+    // EXPECT
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+      })
+    ).toHaveTextContent("Hello World!");
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    coverage: {
+      provider: "c8",
+    },
+  },
+});
