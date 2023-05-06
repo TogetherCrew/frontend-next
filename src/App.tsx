55173ccddae06b9483b5576242a029a3f5dfd36f
zuies <zurabichkhikvadze66@gmail.com>
2023-05-06 16:16:01 +0400

    install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@mui/material';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button variant="outlined" color="primary">
+        dsa
+      </Button>
+    </>
+  );
 }
 
 export default App;
