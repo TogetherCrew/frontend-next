eb71439df91fa1fa4bef86261c3e3b91f8971d6e
zuies <zurabichkhikvadze66@gmail.com>
2023-05-11 14:01:17 +0400

    add react-query

--- src/WrapperApp.tsx
+++ src/WrapperApp.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import { theme } from './constants/theme';
 
+const queryClient = new QueryClient();
+
 export function WrappedApp() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
