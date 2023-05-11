069f7c87a310c80c0925b82f29743c73701c3b90
zuies <zurabichkhikvadze66@gmail.com>
2023-05-11 14:51:12 +0400

    add document

--- src/helper/toasterHelper.ts
+++ src/helper/toasterHelper.ts
@@ -1,9 +1,17 @@
 import { toast, ToastOptions } from 'react-toastify';
 
+/**
+ * Show a toast notification using react-toastify.
+ * @param {string} message - The message to display in the toast.
+ * @param {ToastOptions<Record<string, never>>} options - Optional toast options.
+ */
 export const showToast = (
   message: string,
   options: ToastOptions<Record<string, never>> = {}
 ) => {
+  /**
+   * Default toast options.
+   */
   const defaultOptions: ToastOptions<Record<string, never>> = {
     position: 'bottom-left',
     autoClose: 3000,
@@ -16,9 +24,16 @@ export const showToast = (
     theme: 'light',
   };
 
+  /**
+   * Merge the default options with the provided options.
+   */
   const mergedOptions: ToastOptions<Record<string, never>> = {
     ...defaultOptions,
     ...options,
   };
+
+  /**
+   * Show the toast notification.
+   */
   toast(message, mergedOptions);
 };
